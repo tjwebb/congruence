4149abdb53c15167fc996e38b888a292d8110707
tjwebb <travis@traviswebb.com>
2013-12-29 19:46:41 -0500

    minor refactor

--- congruence.js
+++ congruence.js
@@ -9,16 +9,8 @@ var congruence = (function () {
   function normalizeOptional (key) {
     return key.slice().replace('(?)', '');
   }
-  function wildcard (key) {
-    return (/^\(\+\)/i).test(key);
-  }
-  function normalizeWildcard (key) {
-    return key.slice().replace('(+)', '');
-  }
-  function error (list, err) {
-    if (!_.contains(list, err)) {
-      list.push(err);
-    }
+  function logError (list, err) {
+    if (!_.contains(list, err)) list.push(err);
   }
 
   /**
@@ -32,12 +24,13 @@ var congruence = (function () {
       return _testNode(templateNode, objectNode, errors);
     }
 
-    var templateKeys = _.keys(templateNode),
-      objectKeys = _.keys(objectNode),
-      keyset = _.reject(_.union(templateKeys, objectKeys), function (key) {
-        return (!optional(key) && _.contains(templateKeys, '(?)'+ key)) || key === '(+)';
+    var templateKeys = _.keys(templateNode);
+
+    return _.all(_.map(
+      _.reject(_.union(templateKeys, _.keys(objectNode)), function (key) {
+        return (!optional(key) && _.contains(templateKeys, '(?)' + key)) || key === '(+)';
       }),
-      result = _.map(keyset, function (_key) {
+      function (_key) {
         var subtree, key, oldlen = errors.length;
 
         if (optional(_key)) {
@@ -60,11 +53,9 @@ var congruence = (function () {
             return false;
           }
         }
-
         return subtree;
-      });
-
-    return _.all(result);
+      }
+    ));
   }
 
   /**
@@ -88,24 +79,24 @@ var congruence = (function () {
     if (result) return true;
 
     if (_.isUndefined(predicate)) {
-      error(errors, 'no match for ' + JSON.stringify(value));
+      logError(errors, 'no match for ' + JSON.stringify(value));
     }
     else if (isObjectStrict(predicate) && !isObjectStrict(value)) {
-      error(errors, 'expected (' + value + ') to be an object');
+      logError(errors, 'expected (' + value + ') to be an object');
     }
     else if (_.isRegExp(predicate) && !predicate.test(value)) {
-      error(errors, 'expected ' + predicate + ' to match ' + value);
+      logError(errors, 'expected ' + predicate + ' to match ' + value);
     }
     else if (_.isFunction(predicate) && !predicate(value, errors)) {
       var f = predicate.name || _.find(_.functions(_), function (name) {
         return _[name] == predicate;
       });
       if (value && !_.contains([ 'or', 'not' ], f)) {
-        error(errors, (f || 'anonymous') + '(' + value + ') returned false');
+        logError(errors, (f || 'anonymous') + '(' + value + ') returned false');
       }
     }
     else if (predicate !== value) {
-      error(errors, 'expected (' + predicate + ') to equal ' + value);
+      logError(errors, 'expected (' + predicate + ') to equal ' + value);
     }
 
     return false;
@@ -120,16 +111,15 @@ var congruence = (function () {
    * @example see README
    */
   function test (template, object, _errors) {
-    var errors = _errors || [ ], result;
+    var errors = _errors || [ ];
 
     if (!isObjectStrict(object)) {
-      error(errors, '\'object\' must be a valid js object');
+      logError(errors, '\'object\' must be a valid js object');
     }
     if (!isObjectStrict(template)) {
-      error(errors, '\'template\' must be a valid js object');
+      logError(errors, '\'template\' must be a valid js object');
     }
-
-    return errors.length === 0 && _testSubtree(_.clone(template), object, errors);
+    return !errors.length && _testSubtree(_.clone(template), object, errors);
   }
 
   /**
@@ -166,9 +156,7 @@ var congruence = (function () {
    */
   function not (predicate) {
     return function (value, errors) {
-      var result = !_testSubtree(predicate, value, errors);
-
-      return result;
+      return !_testSubtree(predicate, value, errors);
     };
   }
 
@@ -179,13 +167,10 @@ var congruence = (function () {
    */
   function or () {
     var predicates = _.toArray(arguments);
-
     return function or (value, errors) {
-      var result = _.any(predicates, function (predicate) {
-          return _testSubtree(predicate, value, errors);
-        });
-
-      return result;
+      return _.any(predicates, function (predicate) {
+        return _testSubtree(predicate, value, errors);
+      });
     };
   }
 
